4f5943d0ff298b7e0f87170bf7bd114ffb6f473a
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-12-01 03:18:55 +0100

    connect model Category to model Product and vice versa

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,4 +15,9 @@ class Product extends Model
         'stock',
         'minimum_stock',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
