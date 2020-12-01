8d4d446fa8c5aacc86103422231fb2aaba638c99
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-12-01 04:30:41 +0100

    added CategoryController | function index, store and show implemented

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(Category::with('products')->get(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response(Category::create($request->all()), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+        $category->products;
+        if ($category)
+            return response($category, 200);
+        else
+            return response(null, 204);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,7 @@ class Category extends Model
 
     protected $fillable = [
         'name',
+        'products'
     ];
 
     public function products()
