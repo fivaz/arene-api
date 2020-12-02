ff456076ba7b4f3843ae343b43084a600e9a4d3d
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-12-02 02:10:07 +0100

    added TradCardGameSeeder to the DatabaseSeeder | CategorySeeder uses TradingCardGame

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\TradingCardGame;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class CategorySeeder extends Seeder
 {
@@ -15,11 +18,20 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
+        $trading_card_games = TradingCardGame::all();
+        $tcg_ids = [];
+        foreach ($trading_card_games as $trading_card_game)
+            array_push($tcg_ids, $trading_card_game->id);
+
         Category::factory()
-            ->afterCreating(function (Category $category) {
+            ->afterCreating(function (Category $category) use ($tcg_ids) {
                 Product::factory()
                     ->count(10)
                     ->state(['category_id' => $category->id])
+                    ->state(new Sequence(
+                        ['trading_card_game_id' => Arr::random($tcg_ids)],
+                        ['trading_card_game_id' => null],
+                    ))
                     ->create();
             })
             ->count(5)
