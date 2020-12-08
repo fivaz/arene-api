7c994f98d8ed1491d328f9815bd67c26c90adec6
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-12-08 12:46:43 +0100

    added model Language

---
+++ app/Models/Language.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Language extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
+}
