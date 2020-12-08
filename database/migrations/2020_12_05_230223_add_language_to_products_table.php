439a94df4bd9d69095d7bdf201590a2fb3d0fc65
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-12-08 12:41:29 +0100

    added migration add language_id to products table

---
+++ database/migrations/2020_12_05_230223_add_language_to_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLanguageToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('language_id')
+                ->nullable()
+                ->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['language_id']);
+            $table->dropColumn('language_id');
+        });
+    }
+}
